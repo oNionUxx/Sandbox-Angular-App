41e2039bf62d4ec2b38981e4bb816fff4ede56cf
oNionUxx <omerlubko@gmail.com>
2020-08-14 15:42:47 +0300

    upload to heroku

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,42 +1,37 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-import { PostService } from '../../services/post-service/post.service'
+import { PostService } from '../../services/post-service/post.service';
 import { Post } from '../../models/Post';
-import { toJSDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-calendar';
 
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
-  styleUrls: ['./post-form.component.css']
+  styleUrls: ['./post-form.component.css'],
 })
 export class PostFormComponent implements OnInit {
-  
   @Input() currentPost: Post;
   @Input() isEdit: boolean;
   @Output() newPost: EventEmitter<Post> = new EventEmitter();
   @Output() updatedPost: EventEmitter<Post> = new EventEmitter();
 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addPost(title, body) {
-    if(!title || !body) {
-      alert('Please add post')
-    } else {      
-      this.postService.savePost({title, body} as Post)
-      .subscribe(post => {
+    if (!title || !body) {
+      alert('Please add post');
+    } else {
+      this.postService.savePost({ title, body } as Post).subscribe((post) => {
         this.newPost.emit(post);
       });
     }
   }
 
   updatePost() {
-    this.postService.updatePost(this.currentPost).subscribe( post => {
+    this.postService.updatePost(this.currentPost).subscribe((post) => {
       console.log(post);
       this.isEdit = false;
       this.updatedPost.emit(post);
-    })
+    });
   }
-
 }
